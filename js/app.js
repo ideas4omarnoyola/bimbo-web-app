7c49e81c71c5ab221288a7f7e8fe25cbe0259130
Omar Noyola <omis1296@gmail.com>
2023-05-22 15:42:13 -0600

    :zap: Ajustes rapidos

--- js/app.js
+++ js/app.js
@@ -727,5 +727,27 @@ $(document).ready(function() {
     // ran = generarId();
     // location.hash = "?" + ran;
 
+    // Deshanilitar el clic derecho
+    function disableIE() {
+        if (document.all) {
+            return false;
+        }
+    }
+    function disableNS(e) {
+        if (document.layers || (document.getElementById && !document.all)) {
+            if (e.which==2 || e.which==3) {
+                return false;
+            }
+        }
+    }
+    if (document.layers) {
+        document.captureEvents(Event.MOUSEDOWN);
+        document.onmousedown = disableNS;
+    } 
+    else {
+        document.onmouseup = disableNS;
+        document.oncontextmenu = disableIE;
+    }
+    document.oncontextmenu=new Function("return false");
 
 });
